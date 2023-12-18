411f6263478bfac925b80fafbd9e7bb34d5358db
AmdRzaM <146581643+AmdRzaM@users.noreply.github.com>
2023-12-18 22:19:52 +0330

    Fixes #5-ID-StickyMenu

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//StickyMenu
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+//StickyMenu
